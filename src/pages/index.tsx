9cac3bd95be3961dcdc9c089ba116f3fee42cbe8
Felipe <felipevictor67@hotmail.com>
2021-04-21 16:03:14 -0300

    aula 2 - nlw#5

---
+++ src/pages/index.tsx
@@ -0,0 +1,31 @@
+// SPA 
+/* 
+useEffect(() => {
+    fetch('http://localhost:3333/episodes')
+      .then(response => response.json())
+      .then(data => console.log(data))
+  }, []) 
+*/
+// SSR
+// SSG
+
+export default function Home(props) {
+  return (
+    <div>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
+  )
+}
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  }
+}
